103128bb60e86c012615cdb33377d4596cc2bd28
samevici <victor.samoylenko@dawsoncollege.qc.ca>
2026-04-22 22:44:04 -0400

    would be nice if the form was removed!!!

--- index.php
+++ index.php
@@ -24,37 +24,30 @@
     </head>
     <body>
         <h1> REMOTEFX  Control</h1>
-        <form action="insertion.php" method="GET">
+        
             <p><label for="hi"> High pass %: </label><input type="text" id="hi" name="hi" /> <button onclick="insertValues('hi')">Update</button></p>
-        </form>
+       
 
-        <form action="insertion.php" method="GET">
+   
             <p><label for="band"> Band pass %: </label><input type="text" id="band" name="band" /> <button onclick="insertValues('band')">Update</button></p>
-        </form>
+   
 
-        <form action="insertion.php" method="GET">
+       
             <p><label for="lo"> Low pass %: </label><input type="text" id="lo" name="lo" /> <button onclick="insertValues('lo')">Update</button></p>
-        </form>
 
-        <form action="insertion.php" method="GET">
             <p><label for="rescut"> Resonance cutoff %: </label><input type="text" id="rescut" name="rescut" /> <button onclick="insertValues('rescut')">Update</button></p>
-        </form>
 
-        <form action="insertion.php" method="GET">
+
             <p><label for="resfeed"> Resonance feedback %: </label><input type="text" id="resfeed" name="resfeed" />  <button onclick="insertValues('resfeed')">Update</button></p>
-        </form>
 
-        <form action="insertion.php" method="GET">
             <p><label for="distgain"> Distortion gain %: </label><input type="text" id="distgain" name="distgain" /> <button onclick="insertValues('distgain')">Update</button></p>
-        </form>
+   
 
-        <form action="insertion.php" method="GET">
             <p><label for="distclip"> Distortion clipping %: </label><input type="text" id="distclip" name="distclip" /> <button onclick="insertValues('distclip')">Update</button></p>
-        </form>
-
-        <form action="insertion.php" method="GET">
+        
+      
             <p><label for="volume"> Volume %: </label><input type="text" id="volume" name="volume" /> <button onclick="insertValues('volume')">Update</button></p>
-        </form>
+
 
         <input type="button" onclick="alert('This will eventually do something')" value="EEPROMwrite">
         <input type="button" onclick="alert('This will eventually do something')" value="record">
