751624980747dc87420fa03bebcc5cb6de29a4fa
samevici <victor.samoylenko@dawsoncollege.qc.ca>
2026-04-22 22:26:04 -0400

    attempting to insert data on a per column basis

--- index.php
+++ index.php
@@ -2,40 +2,59 @@
 <html>
     <head>
         <title>REMOTEFX Control</title>
-        <?php
-                 $server = "localhost";
-                $username = "php";
-                $password = "victorphpass";
-                $database = "remotefx";
-                $conn = mysqli_connect($server, $username, $password, $database);
-            
-                // Check for successful connection
-                if (!$conn) {
-                    die("Connection failed: {mysqli_connect_error()}");
-                }
-        
-                ?>
+
         <script>
-            async function getValues() {
+           /* async function getValues() {
                 var xmlHttp = new XMLHttpRequest();
-                xmlHttp.open("GET", "remotevals")
+                xmlHttp.open("GET", "insertion.php?" + this.id + "=" this.innerHTML, false);
+                xmlHttp.send();
+            }*/
+
+           /* async function refreshValues() {
+                 var xmlHttp = new XMLHttpRequest();
+                  xmlHttp.open("GET", "insertion.php?", false);
+                  const result  
+
+
 
             }
+*/
 
-
-        </script>
+        </script> 
     </head>
     <body>
         <h1> REMOTEFX  Control</h1>
-        <form action="response.php" method="POST">
-        <p><label for="hi"> High pass %: </label><input type="text" id="hi" name="hi" /></p>
-        <p><label for="band"> Band pass %: </label><input type="text" id="band" name="band" /></p>
-        <p><label for="lo"> Low pass %: </label><input type="text" id="lo" name="lo" /></p>
-        <p><label for="rescut"> Resonance cutoff %: </label><input type="text" id="rescut" name="rescut" /></p>
-        <p><label for="resfeed"> Resonance feedback %: </label><input type="text" id="resfeed" name="resfeed" /></p>
-        <p><label for="distgain"> Distortion gain %: </label><input type="text" id="distgain" name="distgain" /></p>
-        <p><label for="distclip"> Distortion clipping %: </label><input type="text" id="distclip" name="distclip" /></p>
-        <p><label for="volume"> Volume %: </label><input type="text" id="volume" name="volume" /></p>
+        <form action="insertion.php" method="GET">
+            <p><label for="hi"> High pass %: </label><input type="text" id="hi" name="hi" />   <button type="submit">Update</button></p>
+        </form>
+
+        <form action="insertion.php" method="GET">
+            <p><label for="band"> Band pass %: </label><input type="text" id="band" name="band" /> <button type="submit">Update</button></p>
+        </form>
+
+        <form action="insertion.php" method="GET">
+            <p><label for="lo"> Low pass %: </label><input type="text" id="lo" name="lo" /> <button type="submit">Update</button></p>
+        </form>
+
+        <form action="insertion.php" method="GET">
+            <p><label for="rescut"> Resonance cutoff %: </label><input type="text" id="rescut" name="rescut" /> <button type="submit">Update</button></p>
+        </form>
+
+        <form action="insertion.php" method="GET">
+            <p><label for="resfeed"> Resonance feedback %: </label><input type="text" id="resfeed" name="resfeed" /> <button type="submit">Update</button></p>
+        </form>
+
+        <form action="insertion.php" method="GET">
+            <p><label for="distgain"> Distortion gain %: </label><input type="text" id="distgain" name="distgain" /> <button type="submit">Update</button></p>
+        </form>
+
+        <form action="insertion.php" method="GET">
+            <p><label for="distclip"> Distortion clipping %: </label><input type="text" id="distclip" name="distclip" /> <button type="submit">Update</button></p>
+        </form>
+
+        <form action="insertion.php" method="GET">
+            <p><label for="volume"> Volume %: </label><input type="text" id="volume" name="volume" /> <button type="submit">Update</button></p>
+        </form>
 
         <input type="button" onclick="alert('This will eventually do something')" value="EEPROMwrite">
         <input type="button" onclick="alert('This will eventually do something')" value="record">
---
+++ insertion.php
@@ -0,0 +1,33 @@
+ <?php
+
+                $server = "localhost";
+                $username = "php";
+                $password = "victorphpass";
+                $database = "remotefx";
+                $conn = mysqli_connect($server, $username, $password, $database);
+            
+                // Check for successful connection
+                if (!$conn) {
+                    die("Connection failed: {mysqli_connect_error()}");
+                }
+
+                $key =[];
+                $resp = [];
+
+                foreach ($_GET as $x=>$y)
+                    {
+                        $key[] = "{$x}";
+
+                    };
+                
+                foreach ($_GET as $x=>$y)
+                    {
+                        $resp[] = htmlspecialchars("{$y}");
+
+                    };
+
+
+                $sql = "UPDATE fx_settings SET " . implode ($key) ." = " . implode($resp) . " WHERE id='1';";
+                $result = mysqli_query($conn, $sql);
+
+?>
